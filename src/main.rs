2824931482cde91627e33f84ef865de8d1a64aff
y-not-u <voganwong@hotmail.com>
2022-04-21 11:25:23 +0800

    feat: support to specify config file and delete the proxy

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,27 @@
 use serde_derive::Deserialize;
 use std::collections::HashMap;
 use std::fs;
+use std::path::PathBuf;
 use std::process::Command;
 use structopt::StructOpt;
 use toml;
 
 /// Set a proxy for the other CLI
-#[derive(StructOpt)]
+#[derive(StructOpt, Debug)]
 struct CLI {
+    /// Program wants to proxy
     program: String,
-    url: String,
+
+    /// Proxy URL
+    #[structopt(name = "URL", required_unless("delete"))]
+    url: Option<String>,
+
+    /// Delete the proxy
+    #[structopt(name = "delete", short = "d", long)]
+    delete: bool,
+
+    #[structopt(short, long, parse(from_os_str), default_value = "config.toml")]
+    config: PathBuf
 }
 
 #[derive(Debug, Deserialize)]
@@ -22,41 +34,36 @@ fn main() {
     let args = CLI::from_args();
     let program = args.program;
     let url = args.url;
+    let delete = args.delete;
+    let config_path = args.config;
 
-    let config = read_config();
+    let config = read_config(config_path);
 
     if let Some(proxy) = config.get(&program) {
-        for cmd in proxy.set.iter() {
-            if cmd.contains("{url}") {
-                let cmd = cmd.replace("{url}", url.as_str());
-                run_command(cmd);
+        match delete {
+            true => {
+                for cmd in proxy.unset.iter() {
+                    run_command(cmd.clone());
+                }
+            }
+            false => {
+                if let Some(url) = url {
+                    for cmd in proxy.set.iter() {
+                        if cmd.contains("{url}") {
+                            let cmd = cmd.replace("{url}", url.as_str());
+                            run_command(cmd);
+                        }
+                    }
+                }
             }
         }
     } else {
         println!("Sorry. Program: `{}` is not supported current now", program);
     }
-    // match program.as_str() {
-    //     "npm" => {
-    //         run_command(cmd);
-    //     }
-    //     "yarn" => {
-    //         let cmd = format!("yarn config set proxy {}", url);
-    //         run_command(cmd);
-    //     }
-    //     "git" => {
-    //         let cmd1 = format!("git config http.proxy {}", url);
-    //         run_command(cmd1);
-    //         let cmd2 = format!("git config https.proxy {}", url);
-    //         run_command(cmd2);
-    //     }
-    //     "cargo" => {
-    //         println!("cargo")
-    //     }
-    // }
 }
 
-fn read_config() -> HashMap<String, Proxy> {
-    let toml_string = fs::read_to_string("config.toml").expect("No config file found");
+fn read_config(path: PathBuf) -> HashMap<String, Proxy> {
+    let toml_string = fs::read_to_string(path).expect("No config file found");
     let data: HashMap<String, Proxy> = toml::from_str(&toml_string).unwrap();
 
     data
@@ -64,6 +71,7 @@ fn read_config() -> HashMap<String, Proxy> {
 
 #[cfg(target_os = "windows")]
 fn run_command(cmd: String) {
+    println!("Running: {}", cmd);
     let mut args: Vec<&str> = cmd.split_whitespace().collect();
     args.insert(0, "/c");
 
